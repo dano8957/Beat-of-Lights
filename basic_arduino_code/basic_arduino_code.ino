a9d7181c391b68d26daf0a851f55e69c141c460e
clfchris <clfchris@gmail.com>
2014-12-08 15:15:20 -0700

    my copy of the basic arduino code

--- basic_arduino_code/basic_arduino_code.ino
+++ basic_arduino_code/basic_arduino_code.ino
@@ -1,12 +1,26 @@
-int led = 13;
+int led = 12; ///correspond to ports on arduino breadboard
+int led1 = 8;
+int led2 = 2;
 
 void setup() {
   pinMode(led, OUTPUT);
+  pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);
 }
 
 void loop() {
-  digitalWrite(led, HIGH);
+  digitalWrite(led, HIGH);///HIGH sends current
+  delay(1000);///programs pauses before turning light off (milleseconds)
+  digitalWrite(led,LOW);///LOW turns off current
   delay(1000);
-  digitalWrite(led,LOW);
+  
+  digitalWrite(led1, HIGH);
+  delay(1000);
+  digitalWrite(led1,LOW);
+  delay(1000);
+  
+  digitalWrite(led2, HIGH);
+  delay(1000);
+  digitalWrite(led2,LOW);
   delay(1000);
 }
